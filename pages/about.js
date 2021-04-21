587c5648470f3fce9711bd9807ee1acbe282f305
jorgadev <radovanjorgic14@gmail.com>
2021-04-20 21:19:41 -0700

    Exported project

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import React from "react";
+
+const about = () => {
+  return (
+    <div>
+      <Head>
+        <title>About</title>
+      </Head>
+      <h1>Tempor esse pariatur id nulla quis enim.</h1>
+    </div>
+  );
+};
+
+export default about;
---
+++ pages/contact.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+const contact = () => {
+  return (
+    <div>
+      <Head>
+        <title>Contact</title>
+      </Head>
+      <h1>
+        Cillum nostrud do est Lorem esse incididunt non nostrud veniam ad
+        deserunt et.
+      </h1>
+    </div>
+  );
+};
+
+export default contact;
